7dc2ef41e8d7e65c499fd50fde5581e2205a08a7
Taylor Sather <fivetentaylor@gmail.com>
2012-06-30 19:50:25 -0700

    Did some shit to the chat.
    Reworking the user api a little, the way Dustin suggested. This will
    make it painfully easy for the client developer.

--- public/socket.js
+++ public/socket.js
@@ -1,31 +1,33 @@
 // socket.io specific code
 //var socket = io.connect('http://127.0.0.1:8000');
 
-var Chat = function( chatServer, ready ){
-	if( chatServer )
-		this.socket = io.connect( chatServer );
-	else
-		this.socket = io.connect( chatHost );
+var Chat = function( ready ){
+	this.socket = io.connect( chatHost );
 	this.socket.on( 'connect', ready );
 };
 Chat.prototype.create = function( my_id, their_ids, callback )
 {
 	this.socket.emit( 'start', { 'my_id' : my_id, 'their_ids' : their_ids } );
     this.socket.on( 'new' , function(data){
-    	callback(data.chat_id);
+    	callback({'id' : data.chat_id});
     });
 };
-Chat.prototype.destroy = function( chat_id )
-{
-	this.socket.emit( 'destroy', { 'chat_id' : chat_id } );
+var 
+
+var Chat_Handle = function( chat_id ){
+	this.chat_id = chat_id;
 };
-Chat.prototype.send = function( chat_id, data )
+Chat_Handle.prototype.destroy = function()
 {
-	this.socket.emit( 'data', { 'chat_id' : chat_id, 'data' : data } );
+	this.socket.emit( 'destroy', { 'chat_id' : this.chat_id } );
 };
-Chat.prototype.receive = function( callback )
+Chat_Handle.prototype.message = function( data )
+{
+	this.socket.emit( 'data', { 'chat_id' : this.chat_id, 'data' : data } );
+};
+Chat_Handle.prototype.receive = function( callback )
 {
 	this.socket.on( 'message', function( data ){
-		callback( data.chat_id );
+		callback(data);
 	});
 };
